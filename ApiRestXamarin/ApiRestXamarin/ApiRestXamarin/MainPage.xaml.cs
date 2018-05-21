bf5434890d1ce2e7ce04721c6a2bc9ac2d710476
Jonathan Andres Mideros Narvaez <jonmid.xamarin@gmail.com>
2018-05-21 17:17:48 -0500

    Programacion para listar los usuarios desde la ApiRest

--- ApiRestXamarin/ApiRestXamarin/ApiRestXamarin/MainPage.xaml.cs
+++ ApiRestXamarin/ApiRestXamarin/ApiRestXamarin/MainPage.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using ApiRestXamarin.Model;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -9,9 +13,33 @@ namespace ApiRestXamarin
 {
 	public partial class MainPage : ContentPage
 	{
-		public MainPage()
+
+        private const string Url = "https://jonmidpruebanode.herokuapp.com/users";
+        private readonly HttpClient client = new HttpClient();
+        private ObservableCollection<User> _user;
+
+        public MainPage()
 		{
 			InitializeComponent();
 		}
-	}
+
+        async public void ListData()
+        {
+            string content = await client.GetStringAsync(Url);
+            List<User> users = JsonConvert.DeserializeObject<List<User>>(content);
+            _user = new ObservableCollection<User>(users);
+            listViewUsers.ItemsSource = _user;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            ListData();
+        }
+
+        public void ClickListData(object sender, EventArgs e)
+        {
+            ListData();
+        }
+    }
 }
---
+++ ApiRestXamarin/ApiRestXamarin/ApiRestXamarin/Model/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApiRestXamarin.Model
+{
+    class User
+    {
+        public string Name { get; set; }
+    }
+}
